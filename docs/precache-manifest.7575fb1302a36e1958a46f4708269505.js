8a580144ce8c93e1880edd0655fca2f0163cbb74
Henri Cavalcante <hnri_mxel@hotmail.com>
2019-08-29 08:18:46 -0300

    release

---
+++ docs/precache-manifest.7575fb1302a36e1958a46f4708269505.js
@@ -0,0 +1,172 @@
+self.__precacheManifest = [
+  {
+    "url": "/./assets/79763af9e017b4f0c9b71c696666d2c2.svg"
+  },
+  {
+    "revision": "7cf9f71305ba05a5bc0935a6449e9de6",
+    "url": "/sw.js"
+  },
+  {
+    "url": "/./assets/650eb89a61f307b4855261e2d4769026.svg"
+  },
+  {
+    "url": "/main.2775a25a9feba32a4e71.js"
+  },
+  {
+    "url": "/./assets/f3df5669a32ecca0c9284d9e598ebe0f.svg"
+  },
+  {
+    "url": "/./assets/686f8efa6e3e28e96d1c08399e8d353d.svg"
+  },
+  {
+    "url": "/./assets/a1242e40842339aaa8ee7d810318529c.svg"
+  },
+  {
+    "url": "/./assets/f7d071706faa86e3f5938e5e358a94c8.svg"
+  },
+  {
+    "url": "/./assets/ea69a00de69f7d73e252da457a141f1c.svg"
+  },
+  {
+    "url": "/./assets/d63efcfdc56c853dd75c34c995806ed8.svg"
+  },
+  {
+    "url": "/./assets/e39b39fd14903457ea5f15a79c8cdf19.svg"
+  },
+  {
+    "url": "/./assets/fb5aa800d8ba0ce28930d69e769bd08c.svg"
+  },
+  {
+    "url": "/./assets/6a36d87527d007eb42ebcd5b584b1d14.svg"
+  },
+  {
+    "url": "/./assets/a4565b81fa2ed3072d836ccadaf66936.svg"
+  },
+  {
+    "url": "/./assets/92a5531813f7904890892b57cac4224b.svg"
+  },
+  {
+    "url": "/./assets/6b3eb859db13f41ec7a143ada043521b.svg"
+  },
+  {
+    "url": "/./assets/fbdca1816cc034844bee4dd49290b844.png"
+  },
+  {
+    "url": "/./assets/d0a1c9c5580cdfe8618fcb14bd35db5b.svg"
+  },
+  {
+    "url": "/./assets/7f5fad272544064b051f72ec1b1e50f8.png"
+  },
+  {
+    "url": "/./assets/fc8568286ceb8934e1dbd97878c6b813.svg"
+  },
+  {
+    "url": "/./assets/8557b1c88ab0e6293e2a475df28d63f8.png"
+  },
+  {
+    "url": "/./assets/f4617df97e3c407b650782ac311a8e8e.png"
+  },
+  {
+    "url": "/./assets/bc97233b76ad7f40f5ad5f3cd845146b.svg"
+  },
+  {
+    "url": "/./assets/f94d174b76cd13c2d68a4be1f875c21a.svg"
+  },
+  {
+    "url": "/./assets/e62c78334f73208dc2208a32333e6649.svg"
+  },
+  {
+    "url": "/./assets/986cae537511396d453fcc868f28b40d.svg"
+  },
+  {
+    "url": "/./assets/db9158f80ba30cc2ecb9b6f110bd3008.svg"
+  },
+  {
+    "url": "/./assets/af25ae9de2a99874c01e577d200a256f.svg"
+  },
+  {
+    "revision": "f20d23124c35acadce7b",
+    "url": "/abb693434eb529e83ee6593a485b0bac.css"
+  },
+  {
+    "url": "/./assets/f3418a443e7d841097c714d69ec4bcb8.ico"
+  },
+  {
+    "url": "/./assets/6b6fa6e3a15fb6d5ab5f0b83dcbeee04.png"
+  },
+  {
+    "url": "/./assets/6aea037b38e0797798242a320cd58563.svg"
+  },
+  {
+    "url": "/./assets/9c6798a4edb1855436e6c04acabdec55.svg"
+  },
+  {
+    "url": "/./assets/cb1f9142eaf3572371da01495072f708.svg"
+  },
+  {
+    "url": "/./assets/c06b3dc43ae8b282308bfe7e849deab6.svg"
+  },
+  {
+    "url": "/./assets/f3746648cb2890f54b0957201ff5b801.svg"
+  },
+  {
+    "url": "/./assets/68ce0559cbb80999b27fe112c1d87c60.svg"
+  },
+  {
+    "url": "/./assets/b8ecabae8dee6e7e8385c5d1a60bc8eb.svg"
+  },
+  {
+    "url": "/./assets/fdd813f62eb76a1db16bff890d27e0a1.svg"
+  },
+  {
+    "url": "/./assets/cf26052694f03d9b411eb9660288d69b.svg"
+  },
+  {
+    "url": "/./assets/3729f9be917f49c1ea85a2b50e1bcdec.svg"
+  },
+  {
+    "url": "/./assets/3b82b8e7b3098e434d8d7d1ffd489c62.png"
+  },
+  {
+    "url": "/./assets/4660c2c5640dd743f20ace85d200899f.svg"
+  },
+  {
+    "url": "/./assets/50d6f025d54bf332a8d1259761429444.png"
+  },
+  {
+    "url": "/./assets/0ee829d1ab215c560bec6972f9c5aba3.svg"
+  },
+  {
+    "url": "/./assets/141bfdc0c4530e96dc8e4df108c1e17f.svg"
+  },
+  {
+    "url": "/./assets/1cc0e2b78ca7c549932529c4f3419838.svg"
+  },
+  {
+    "url": "/./assets/1aa8f3ea786d9bc321a1c6ac319034af.svg"
+  },
+  {
+    "url": "/./assets/276048e1cb460d3fafc5fb4c9b65d2f4.svg"
+  },
+  {
+    "url": "/./assets/31909d5146804bbfd87d9c1477a9c1ea.svg"
+  },
+  {
+    "url": "/./assets/1040749f9476348718b8f6e0e20dc921.jpg"
+  },
+  {
+    "url": "/./assets/0cbad2755d65b5b2ceda5a01eca46ac3.svg"
+  },
+  {
+    "url": "/./assets/061f9d74da03353422938f2d5222fd97.svg"
+  },
+  {
+    "url": "/./assets/053807d8fb48a51958bc70fb39a44ed5.svg"
+  },
+  {
+    "url": "/./assets/03e18dd5d026c71e14c85ece87f97173.png"
+  },
+  {
+    "url": "/./assets/02bd81d276459d1fc1d10f3eadcc1631.svg"
+  }
+];
--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.dad701177339cd9f42a1a38398a3e2ed.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+importScripts("/precache-manifest.7575fb1302a36e1958a46f4708269505.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 const CACHE_VERSION = 'v10';
 const initialCache = [
