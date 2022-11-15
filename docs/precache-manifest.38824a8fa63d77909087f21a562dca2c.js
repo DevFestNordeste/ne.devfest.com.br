8693f01f5c637c5032fc47a79b4d10e8cd0895f5
Antonio Pedro Ferreira <apsf88@gmail.com>
2022-11-15 06:11:05 -0300

    incluindo datas e botao para comprar ingresso em evidencia na home do site

--- docs/precache-manifest.38824a8fa63d77909087f21a562dca2c.js
+++ docs/precache-manifest.38824a8fa63d77909087f21a562dca2c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = [
   {
-    "url": "/main.e91164af7c92f9ab7a1c.js"
+    "url": "/main.53002529f79ac9d13b23.js"
   },
   {
     "revision": "f3949cd4c8d913f14dba",
@@ -38,21 +38,6 @@ self.__precacheManifest = [
   },
   {
     "url": "/./assets/c21f2553dc95ccaca56869cb590551ad.png"
-  },
-  {
-    "url": "/./assets/686f8efa6e3e28e96d1c08399e8d353d.svg"
-  },
-  {
-    "url": "/./assets/02bd81d276459d1fc1d10f3eadcc1631.svg"
-  },
-  {
-    "url": "/./assets/22eaeb0ff81a073cd0a01ca311ab4b62.png"
-  },
-  {
-    "url": "/./assets/d42b9ad23f020a015980f53db1d4a8e6.png"
-  },
-  {
-    "url": "/./assets/72e109f72f6dd0fb6dc03f5912f49e64.png"
   },
   {
     "url": "/./assets/f7d071706faa86e3f5938e5e358a94c8.svg"
@@ -170,6 +155,21 @@ self.__precacheManifest = [
   },
   {
     "url": "/./assets/141bfdc0c4530e96dc8e4df108c1e17f.svg"
+  },
+  {
+    "url": "/./assets/686f8efa6e3e28e96d1c08399e8d353d.svg"
+  },
+  {
+    "url": "/./assets/02bd81d276459d1fc1d10f3eadcc1631.svg"
+  },
+  {
+    "url": "/./assets/22eaeb0ff81a073cd0a01ca311ab4b62.png"
+  },
+  {
+    "url": "/./assets/d42b9ad23f020a015980f53db1d4a8e6.png"
+  },
+  {
+    "url": "/./assets/72e109f72f6dd0fb6dc03f5912f49e64.png"
   },
   {
     "url": "/./assets/c06b3dc43ae8b282308bfe7e849deab6.svg"
--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.805a9e2cc29a86e378144f6e87da8f95.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+importScripts("/precache-manifest.38824a8fa63d77909087f21a562dca2c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 const CACHE_VERSION = 'v10';
 const initialCache = [
