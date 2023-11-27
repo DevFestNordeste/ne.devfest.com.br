decc96179cc66ed429487d58eec2a3f39114ec5a
Nicoly-Almeida <nicolyejady@gmail.com>
2023-11-27 14:04:13 -0300

    carrossel

--- src/app/pages/programacao/programacao.component.ts
+++ src/app/pages/programacao/programacao.component.ts
@@ -42,111 +42,106 @@ export class ProgramacaoComponent {
     },
   ]
 
-  items_tarde = [
-    {
-      horario: "14:00 - 14:40",
-      children: [
-        {
-          sala: "Sala Verde",
-          titulo: "Modelos de IA",
-          palestrante: "Cecília Flávia | Cientista de Dados - Ambev Tech",
-          color: "#34A853"
-        },
-        {
-          sala: "Sala Amarela",
-          titulo: "Acessibilidade para além das PcDs: uso de comando de voz por Apicultores",
-          palestrante: "Virgílio Magalhães  &  Jorge Kavicki | Agrobee",
-          color: "#F9AB00"
-        },
-        {
-          sala: "Sala Azul",
-          titulo: "Desenvolvimento de Games com ReactJs",
-          palestrante: "Mackson Araújo | Tech Lead - Grupo Neiva",
-          color: "#4285F4"
-        },
-        {
-          sala: "Sala Vermelha",
-          titulo: "Como você pode proteger seus apps e seus usuários?",
-          palestrante: "Fausto Blanco | Software Engineer Sênior - Banco BV",
-          color: "#EA4335"
-        },
-        {
-          sala: "Sala Branca",
-          titulo: "Ao infinito e além? Afinal, as IAs têm limite?",
-          palestrante: "Andressa Freires | Data Scientist Sênior - Nubank",
-        },
-      ]
-    },
-    {
-      horario: "14:40 - 15:20",
-      children: [
-        {
-          sala: "Sala Verde",
-          titulo: "Construindo Apps Android de Qualidade para o Futuro com Integração Contínua",
-          palestrante: "Aline Ayres | Lead Mobile Developer - Thoughtworks",
-          color: "#34A853"
-        },
-        {
-          sala: "Sala Amarela",
-          titulo: "Como sobreviver ao Largados & Pelados com um Autoencoder",
-          palestrante: "Arnaldo Gualberto | Machine Learning Engineer Sr - Arado",
-          color: "#F9AB00"
-        },
-        {
-          sala: "Sala Azul",
-          titulo: "Automações Mobile com Github Actions",
-          palestrante: "Ademir Queiroga | Android & Automation engineer - Fuze",
-          color: "#4285F4"
-        },
-        {
-          sala: "Sala Vermelha",
-          titulo: "Desvendando a síndrome do impostor: Seja você, sem Desculpas!",
-          palestrante: "Isabelle Melo &  Antônio Pedro | Grupo Neiva & Picpay",
-          color: "#EA4335"
-        },
-        {
-          sala: "Sala Branca",
-          titulo: "Java X Kotlin: simplificando o backend",
-          palestrante: "Francisco Neto | Integrante do projeto Synchro GoSyn4Tax",
+  children1 = [
+      {
+        sala: "Sala Verde",
+        titulo: "Modelos de IA",
+        palestrante: "Cecília Flávia | Cientista de Dados - Ambev Tech",
+        color: "#34A853"
+      },
+      {
+        sala: "Sala Amarela",
+        titulo: "Acessibilidade para além das PcDs: uso de comando de voz por Apicultores",
+        palestrante: "Virgílio Magalhães  &  Jorge Kavicki | Agrobee",
+        color: "#F9AB00"
+      },
+      {
+        sala: "Sala Azul",
+        titulo: "Desenvolvimento de Games com ReactJs",
+        palestrante: "Mackson Araújo | Tech Lead - Grupo Neiva",
+        color: "#4285F4"
+      },
+      {
+        sala: "Sala Vermelha",
+        titulo: "Como você pode proteger seus apps e seus usuários?",
+        palestrante: "Fausto Blanco | Software Engineer Sênior - Banco BV",
+        color: "#EA4335"
+      },
+      {
+        sala: "Sala Branca",
+        titulo: "Ao infinito e além? Afinal, as IAs têm limite?",
+        palestrante: "Andressa Freires | Data Scientist Sênior - Nubank",
+      },
+    ]
 
-      
-        },
-      ]
-    },
-    {
-      horario: "15:20 - 16:00",
-      children: [
-        {
-          sala: "Sala Verde",
-          titulo: "O Renascimento do Angular",
-          palestrante: "Gustavo Costa | Front-End Sênior",
-          color: "#34A853"
-        },
-        {
-          sala: "Sala Amarela",
-          titulo: "IA Generativa: será que a Skynet vem aí?",
-          palestrante: "Jéssica Costa | Cientista de Dados - Deloitte",
-          color: "#F9AB00"
-        },
-        {
-          sala: "Sala Azul",
-          titulo: "Desvendando apicuns por meio de Google Earth Engine e Deep Learning",
-          palestrante: "Maria Luize | Deep Learning Researcher - Solved Soluções",
-          color: "#4285F4"
-        },
-        {
-          sala: "Sala Vermelha",
-          titulo: "Do Caos à Clareza: a Magia das MLOps na Deduplicação de Dados",
-          palestrante: "Rodolfo Teles | Engenheiro Machine Learning - Globo",
-          color: "#EA4335"
-        },
-        {
-          sala: "Sala Branca",
-          titulo: "Gestão de tempo para alcançar grandes metas",
-          palestrante: "Duda Pinheiro | Product Owner - Fcamara",
-        },
-      ]
-    },
+    children2 = [
+      {
+        sala: "Sala Verde",
+        titulo: "Construindo Apps Android de Qualidade para o Futuro com Integração Contínua",
+        palestrante: "Aline Ayres | Lead Mobile Developer - Thoughtworks",
+        color: "#34A853"
+      },
+      {
+        sala: "Sala Amarela",
+        titulo: "Como sobreviver ao Largados & Pelados com um Autoencoder",
+        palestrante: "Arnaldo Gualberto | Machine Learning Engineer Sr - Arado",
+        color: "#F9AB00"
+      },
+      {
+        sala: "Sala Azul",
+        titulo: "Automações Mobile com Github Actions",
+        palestrante: "Ademir Queiroga | Android & Automation engineer - Fuze",
+        color: "#4285F4"
+      },
+      {
+        sala: "Sala Vermelha",
+        titulo: "Desvendando a síndrome do impostor: Seja você, sem Desculpas!",
+        palestrante: "Isabelle Melo &  Antônio Pedro | Grupo Neiva & Picpay",
+        color: "#EA4335"
+      },
+      {
+        sala: "Sala Branca",
+        titulo: "Java X Kotlin: simplificando o backend",
+        palestrante: "Francisco Neto | Integrante do projeto Synchro GoSyn4Tax",
+
+    
+      },
+    ]
+
+    children3 = [
+      {
+        sala: "Sala Verde",
+        titulo: "O Renascimento do Angular",
+        palestrante: "Gustavo Costa | Front-End Sênior",
+        color: "#34A853"
+      },
+      {
+        sala: "Sala Amarela",
+        titulo: "IA Generativa: será que a Skynet vem aí?",
+        palestrante: "Jéssica Costa | Cientista de Dados - Deloitte",
+        color: "#F9AB00"
+      },
+      {
+        sala: "Sala Azul",
+        titulo: "Desvendando apicuns por meio de Google Earth Engine e Deep Learning",
+        palestrante: "Maria Luize | Deep Learning Researcher - Solved Soluções",
+        color: "#4285F4"
+      },
+      {
+        sala: "Sala Vermelha",
+        titulo: "Do Caos à Clareza: a Magia das MLOps na Deduplicação de Dados",
+        palestrante: "Rodolfo Teles | Engenheiro Machine Learning - Globo",
+        color: "#EA4335"
+      },
+      {
+        sala: "Sala Branca",
+        titulo: "Gestão de tempo para alcançar grandes metas",
+        palestrante: "Duda Pinheiro | Product Owner - Fcamara",
+      },
+    ]
+
+
+  items_tarde = [
     {
       horario: "16:00 - 16:15",
       titulo: "Avisos/ Sorteios"
@@ -175,4 +170,22 @@ export class ProgramacaoComponent {
   troca(){
     this.horario = !this.horario
   }
+
+  responsiveOptions = [
+    {
+      breakpoint: '1199px',
+      numVisible: 3,
+      numScroll: 1
+  },
+    {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1
+    },
+    {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1
+    }
+  ];
 }
